235b7c348db256213198a3a6a388f4109cdb5bcf
Paul Freeman <paul@mojo-projects.com>
2013-04-04 18:33:24 +0100

    Initial commit of a basic bit of code (untested)

---
+++ basic.php
@@ -0,0 +1,99 @@
+<?php
+
+define(DEBUG,true); // Turn debug output on and off here.
+
+// Based on the specification at http://www.atoc.org/clientfiles/File/RSPS5004%20v27.pdf
+
+$handle = @fopen("/tmp/inputfile.txt", "r");
+if ($handle) {
+    while (($buffer = fgets($handle, 4096)) !== false) {
+        echo $buffer;
+
+        $type = substr($buffer, 0, 2); // First Two Characters Are the type code
+        switch($type){
+        	/**
+        	 * 4.1 Header Record (HD)
+        	 * 
+			 * Field Name 				Start 	Size Format Comment 
+			 * Record Identity			0 		2    A 		value HD 
+			 * File Mainframe Identity 	2 		2 	 A 		See note (1)
+			 * Date of Extract 			4 		6	 N 		Format ddmmyy. See note (2)
+			 * Time of Extract 			10 		4 	 N 		Format hhmm. 24-hour clock
+			 * Current-File-Ref 		14 		7 	 A 		Unique file reference. See note (1)
+			 * Last-File-Ref 			21 		7 	 A 		Last file generated ref. See note (1)
+			 * Bleed-off/Update Ind 	28 		1 	 A 		U = update extract F = full extract
+			 * Version					29 		1 	 A 		Version of CIF software, incremented for each significant change.
+			 * User Extract Start Date 	30 		6 	 N 		Format ddmmyy. Today's date if user time window is number of days. See note (2)
+			 * User Extract End Date 	36		6 	 N 		Format ddmmyy. See note (2)
+			 * Spare 					42		20 	 A
+        	 */
+        	case 'HD': {
+        		$header = array();
+        		$header['recordIdentity'] = substr($buffer, 0, 2); // Will always be HD
+        		$header['fileMainframeIdentity'] = trim( substr($buffer, 2, 2) );
+        		$header['dateOfExtract'] = trim( substr($buffer, 4, 6) );
+        		$header['timeOfExtract'] = trim( substr($buffer, 10, 4) );
+        		$header['currentFileRef'] = trim( substr($buffer, 14, 7) );
+        		$header['lastFileRef'] = trim( substr($buffer, 21, 7) );
+        		$header['bleedOff'] = trim( substr($buffer, 28, 1) );
+        		$header['version'] = trim( substr($buffer, 29, 1) );
+        		$header['userExtractStartDate'] = trim( substr($buffer,	30, 6) );
+        		$header['userExtractEndDate']	= trim( substr($buffer, 36, 6) );
+        		// We don't bother with the spare data.
+
+        		if(DEBUG){
+        			echo "\nHD: \n\n"
+        			print_r($header);
+        		}
+
+        		break;
+        	}
+        	// TIPLOC
+        	case 'TI': {
+        		break;
+        	}
+        	case 'TA': {
+
+        	}
+        	case 'TD': {
+
+        	}
+        	// Association Records
+        	case 'AA': {
+
+        	}
+        	// Train Scheduals
+        	case 'BS': { # a basic schedule record
+
+        	}
+        	case 'BX': { # a basic schedule extra details record
+
+        	}
+        	case 'TN': { # train specific note records, if present
+
+        	}
+        	case 'LO': { # an origin location record 
+
+        	}
+        	case 'LI': { # all intermediate location records in journey sequence
+
+        	}
+        	case 'CR': { #preceded by a Change en Route, if present, for the location
+
+        	}
+        	case 'LT': { # terminating location record
+
+        	}
+        	case 'LN': { # location specific note records (These may follow any LO, LI or LT records)
+
+        	}
+
+        }
+
+    }
+    if (!feof($handle)) {
+        echo "Error: unexpected fgets() fail\n";
+    }
+    fclose($handle);
+}
+?>
