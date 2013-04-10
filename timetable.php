ce350e325bb3015567be0293a83e97f7b0365b39
Paul Freeman <paul@mojo-projects.com>
2013-04-10 16:36:41 +0100

    Timetable setup via the web via the public folder

--- timetable.php
+++ timetable.php
@@ -7,22 +7,31 @@ $db = $mongo->trains;
 $timetables = $db->timetables;
 $locations = $db->locations;
 
-$_GET['from'] = 'STM';
-$_GET['to'] = 'LVC';
+$errors = array();
 
-if($_GET['from']) {
+if( isset( $_GET['from'] ) ) {
 	preg_match('/^[A-Z]{0,3}/', strtoupper($_GET['from']), $fromResult);
 	$from = $fromResult[0];
+} else {
+	$errors[] = '"from" not set.';
 }
-if($_GET['to']) {
+if( isset( $_GET['to'] ) )  {
 	preg_match('/^[A-Z]{0,3}/', strtoupper($_GET['to']), $toResult);
 	$to = $toResult[0];
+} else {
+	$errors[] = '"to" not set.';
+}
+
+if( count($errors) )
+{
+	header('Content-Type: application/json');
+	die( json_encode($errors) );
 }
 
 $fromLocation = $locations->find(['crs' => $from])->getNext();
 $toLocation = $locations->findOne(['crs' => $to]);
-print_r($fromLocation);
-echo "Searching Train From: ".$fromLocation['tpsDescription']." To ".$toLocation['tpsDescription']."\n";
+
+//echo "Searching Train From: ".$fromLocation['tpsDescription'] . "(" . $fromLocation['name'] . ") To ".$toLocation['tpsDescription']. "(" . $toLocation['name'] . ") \n";
 
 // Example: db.timetables.find( { $and: [ { "stops.location":"STMCHLS" } ], "stops.location": "LVRPLCH" } );
 // TODO: Better way to structure query?
@@ -35,4 +44,8 @@ foreach($result as $document){
 	$docArray[] = $document;
 }
 
+$docArray['from'] = $fromLocation;
+$docArray['to'] = $toLocation;
+
+header('Content-Type: application/json');
 echo json_encode($docArray);
